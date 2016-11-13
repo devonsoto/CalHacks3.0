df27b0533ed41dcbdfbc1848d5d68e7ad249aed9
MiRamirezJr <miramirezjr@gmail.com>
2016-11-13 08:00:53 -0800

    Update spending.data.py

--- spending.data.py
+++ spending.data.py
@@ -2,17 +2,21 @@ import requests
 import json
 from pprint import pprint
 
-def withdraw( amount ):
-    customerId = '58282f94360f81f10454bd2a'
+def withdraw( amount, debt ):
+    amount = amount - debt;
+    return amount;
+
+def creAccount( cusNum, bal ):
+    customerId = cusNum
     apiKey = '18f3a2575d68ead99755eb8f54bf9d66'
 
-    url = 'http://api.reimaginebanking.com/accounts/58283089360f81f10454bd2f/purchases?key=18f3a2575d68ead99755eb8f54bf9d66'.format(customerId,apiKey)
+    url = 'http://api.reimaginebanking.com/customers/58282f94360f81f10454bd2a/accounts?key=18f3a2575d68ead99755eb8f54bf9d66'.format(customerId,apiKey)
     payload = {
-    "merchant_id": "57cf75cea73e494d8675ec49",
-    "medium": "balance",
-    "purchase_date": "2016-11-12",
-    "amount": amount,
-    "description": "string"
+    "type": "Checking",
+    "nickname": "DeSo",
+    "rewards": 0,
+    "balance": bal,
+    "account_number": "1234567891012136"
     }
     # Create a customer
     response = requests.post(
@@ -20,59 +24,21 @@ def withdraw( amount ):
 	   data=json.dumps(payload),
 	   headers={'content-type':'application/json'},
         )
-
-def deposit( amount ):
-    customerId = '58282f94360f81f10454bd2a'
-    apiKey = '18f3a2575d68ead99755eb8f54bf9d66'
-
-    url = 'http://api.reimaginebanking.com/accounts/58283089360f81f10454bd2f/deposits?key=18f3a2575d68ead99755eb8f54bf9d66'.format(customerId,apiKey)
-    payload = {
-    "medium": "balance",
-    "transaction_date": "2016",
-    "amount": amount,
-    "description": "initial balance"
-    }
-    # Create a customer
-    response = requests.post(
-	   url,
-	   data=json.dumps(payload),
-	   headers={'content-type':'application/json'},
-       )
     data1 = response.json();
     return data1;
 
-def getAccount( accNum ):
-    customerId = '58282f94360f81f10454bd2a'
-    apiKey = '18f3a2575d68ead99755eb8f54bf9d66'
-
-    url = 'http://api.reimaginebanking.com/accounts/58283089360f81f10454bd2f?key=18f3a2575d68ead99755eb8f54bf9d66'.format(customerId,apiKey)
-    payload = {
-    "_id": accNum
-    }
-    # Create a customer
-    response = requests.get(
-	   url,
-	   data=json.dumps(payload),
-	   headers={'content-type':'application/json'},
-        )
-    data1 = response.json();
-    return data1;
-
+customerId = "58282f94360f81f10454bd2a"
+Bal = int(input());
 aNum = "58283089360f81f10454bd2f";
 textParameter = 1000;
-tBalance = getAccount(aNum);
-zero = tBalance['balance'];
-withdraw(zero);
-inBal = 4000;
-deposit(inBal);
 list = [50, 400, 1200];
 
 for i in range(0,3):
-	amount = list[i];
-	withdraw(amount);
-	if(list[i] > 1000):
+    amount = list[i];
+    Bal = withdraw(Bal, amount);
+	if(amount > textParameter):
 		
 
-fAData = getAccount(aNum);
-fBal = fAData['balance'];
+cData = creAccount(customerId, Bal);
+fBal = cData['objectCreated']['balance'];
 print(fBal);
